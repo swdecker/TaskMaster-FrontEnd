a7ced0c08b04bacc583c940c5a7ae5c45742c5d8
github username <github email address>
2019-12-04 09:49:54 -0600

    initial map rendered

--- src/containers/TasksContainer.js
+++ src/containers/TasksContainer.js
@@ -1,43 +1,47 @@
-import React, { Component } from 'react';
-import Task from '../components/Task'
-
+import React, { Component } from "react";
+import Task from "../components/Task";
+import MapContainer from "./MapContainer.js";
 
 class TasksContainer extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       tasks: []
-    }
+    };
   }
 
-  componentDidMount(){
-    fetch('/api/tasks')
-    .then(response => {return response.json()})
-    .then(taskData => { this.setState({
-      tasks: taskData
-    })
-    console.log(this.state.tasks)
-
-    })
+  componentDidMount() {
+    fetch("/api/tasks")
+      .then(response => {
+        return response.json();
+      })
+      .then(taskData => {
+        this.setState({
+          tasks: taskData
+        });
+      });
   }
-
-
 
   render() {
-      return (
+    return (
+      <div>
+        <h2 align="center"> Tasks </h2>
+        {this.state.tasks.map(task => (
+          <Task
+            key={task.id}
+            id={task.id}
+            name={task.name}
+            description={task.description}
+            priority={task.priority}
+            duration={task.duration}
+          />
+        ))}
         <div>
-          <h2 align="center"> Tasks </h2>
-          {this.state.tasks.map(task => (<Task
-              id={task.id}
-              name={task.name}
-              description={task.description}
-              priority={task.priority}
-              duration={task.duration}/>))}
+          <MapContainer />
         </div>
-            )
-          }
+      </div>
+    );
+  }
 }
-
-
 
 export default TasksContainer;
